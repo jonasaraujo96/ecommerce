9e482beea5fccf291595095dc0499dcaad124459
jonasaraujo96 <jonasaraujo96@gmail.com>
2019-09-08 17:52:11 -0300

    landing page

---
+++ js/products.js
@@ -0,0 +1,99 @@
+$(function () {
+  const theTemplateScript = $("#navbar-template").html();
+
+  const theTemplate = Handlebars.compile(theTemplateScript);
+
+  const context = {
+    buttons: [
+      {
+        icon: 'home',
+        name: 'Página inicial',
+        link: '../index.html'
+      },
+      {
+        icon: 'tshirt',
+        name: 'Produtos',
+        link: 'produtos.html'
+      },
+      {
+        icon: 'phone',
+        name: 'Contato',
+        link: 'contact.html'
+      },
+      {
+        icon: 'info-circle',
+        name: 'Sobre',
+        link: 'about.html'
+      },
+    ]
+  };
+
+  const theCompiledHtml = theTemplate(context);
+
+  $(document.body).append(theCompiledHtml);
+});
+
+
+$(function () {
+  const theTemplateScript = $("#banner-template").html();
+  const theTemplate = Handlebars.compile(theTemplateScript);
+  const context = {};
+  const theCompiledHtml = theTemplate(context);
+  $(document.body).append(theCompiledHtml);
+});
+
+$(function () {
+  const theTemplateScript = $("#grid-template").html();
+  const theTemplate = Handlebars.compile(theTemplateScript);
+  const context = {
+    itens: [
+      {
+        name: 'Saia 1',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        price: 'R$ 50,00'
+      },
+      {
+        name: 'Saia 2',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        price: 'R$ 30,00'
+      },
+      {
+        name: 'Saia 3',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        price: 'R$ 25,90'
+      },
+      {
+        name: 'Saia 4',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        price: 'R$ 19,99'
+      },
+      {
+        name: 'Saia 5',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        price: 'R$ 63,00'
+      },
+      {
+        name: 'Saia 6',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        price: 'R$ 19,90'
+      },
+      {
+        name: 'Saia 7',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        price: 'R$ 21,90'
+      },
+      {
+        name: 'Saia 8',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        price: 'R$ 19,90'
+      },
+    ]
+  };
+  const theCompiledHtml = theTemplate(context);
+  $(document.body).append(theCompiledHtml);
+});
+
+function openMenu() {
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('active');
+}
